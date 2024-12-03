57918f513c7a1a34e5ee3eb27798f71d44a973d4
shamim150 <smshamimmahmud150@gmail.com>
2024-12-03 18:29:09 +0600

    add

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className= 'w-full h-full grid grid-cols-5 grid-rows-3 gap-5 '>
+    <div className= 'w-full h-full grid grid-cols-5 grid-rows-3 gap-5  rounded-3xl'>
       <div className='bg-[#32e969] rounded-3xl overflow-hidden text-center'>1</div>
-      <div className='bg-[#cc6767] notification relative text-center'>2</div>
+      <div className='bg-[#cc6767] notification relative text-center rounded-3xl'>2</div>
       <div className='bg-[#5cc23c] rounded-3xl overflow-hidden text-center'>3</div>
       <div className='bg-[#3e339e] rounded-3xl overflow-hidden text-center'>4</div>
       <div className='bg-[#c03333] rounded-3xl overflow-hidden text-center'>5</div>
